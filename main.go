58ed5da4cb0bc36ac5fdb323f3e7e6228dbfdf40
Matthew Hartman <hartman.m@pg.com>
2021-03-09 15:48:44 -0500

    more work

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/rekognition"
 	"github.com/tidwall/gjson"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type videoMetaData struct {
 	Width float64
 	Height float64
-	FrameRate string
+	FrameRate float64
 	Duration float64
 	DurationTS int
 	FrameCount int
@@ -20,16 +21,64 @@ type videoMetaData struct {
 func main(){
 
 	// Get Metadata
-	a, err := ffmpeg.Probe("/Users/hartmamt/Projects/pg/videoSplitter/in/bezos_vogels.mp4")
+	a, err := ffmpeg.Probe(
+		"/Users/hartmamt/Projects/pg/videoSplitter/in/durst.mp4",
+		ffmpeg.KwArgs{},
+		)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(a)
+
+	/*
+	{
+	            "BoundingBox": {
+	                "Width": 0.06047183275222778,
+	                "Height": 0.16532716155052185,
+	                "Left": 0.4213453233242035,
+	                "Top": 0.39640137553215027
+	            },
+
+
+	"BoundingBox": {
+		"Width": 0.12551015615463257,
+			"Height": 0.2838538885116577,
+			"Left": 0.6826857328414917,
+			"Top": 0.2881534993648529
+	},
+*/
+
+	//b, err := ioutil.ReadFile("/Users/hartmamt/Projects/pg/videoSplitter/in/durst.json") // just pass the file name
+	//if err != nil {
+	//	fmt.Print(err)
+	//}
+//#(nets.#(=="fb"))#
+//(Face.Confidence.#(>50.0))#.#
+//{name.first,age,"the_murphys":friends.#(last="Murphy")#.first}
+//	result := gjson.Get(string(b), "{Persons.#.Timestamp,\"faces\":Persons.#.Person.Face}") // print the content as 'bytes'
+//
+//	result.ForEach(func(key, value gjson.Result) bool {
+//		//fmt.Println(key)
+//		println(value.String())
+//		return false
+//		return true // keep iterating
+//	})
+
+//	return
+	//str := string(b) // convert content to a 'string'
+	/*
+	ffprobe video.mp4 -select_streams v -show_entries frame=coded_picture_number,pkt_pts_time -of csv=p=0:nk=1 -v 0
+	 */
+
+	//fmt.Println(a)
+	frameRate := gjson.Get(a, "streams.0.r_frame_rate").String()
+	calculatedFrameRateA, _ := strconv.ParseFloat(strings.Split(frameRate,"/")[0],64)
+	calculatedFrameRateB, _ := strconv.ParseFloat(strings.Split(frameRate,"/")[1], 64)
+
 	metaData := videoMetaData{
 		Width: gjson.Get(a, "streams.0.width").Float(),
 		Height: gjson.Get(a, "streams.0.height").Float(),
-		FrameRate: gjson.Get(a, "streams.0.height").String(),
+		FrameRate: calculatedFrameRateA/calculatedFrameRateB,
 		Duration: gjson.Get(a, "streams.0.duration").Float(),
 		DurationTS: int(gjson.Get(a, "streams.0.duration_ts").Int()),
 		FrameCount: int(gjson.Get(a, "streams.0.nb_frames").Int()),
@@ -37,134 +86,88 @@ func main(){
 
 	fmt.Printf("%v", metaData)
 
-	/*
-	          "BoundingBox": {
-	            "Width": 0.02242584154009819,
-	            "Height": 0.05018870159983635,
-	            "Left": 0.23596711456775665,
-	            "Top": 0.43446972966194153
-	          },
-	 */
-
-	/*
-		ffmpeg -i derpdog.mp4 -filter_complex \
-		 "[0:v]crop=200:200:60:30,boxblur=10[fg]; \
-		  [0:v][fg]overlay=60:30[v]" \
-		-map "[v]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart derpdogblur.mp4
-	*/
-
-	box := rekognition.BoundingBox{
-		Height: aws.Float64(0.4016371965408325),
-		Left: aws.Float64(0.42521291971206665),
-		Top: aws.Float64(0.17050546407699585),
-		Width: aws.Float64(0.1513211727142334),
-	}
-
-
-
-	// Break Into Frames
-	err = ffmpeg.
-		Input("/Users/hartmamt/Projects/pg/videoSplitter/in/bezos_vogels.mp4").
-		Filter("fps", ffmpeg.Args{"30/1"}).
-		Output("/Users/hartmamt/Projects/pg/videoSplitter/out/test-00%d.jpg", ffmpeg.KwArgs{"start_number": 0}).
-		OverWriteOutput().
-		Run()
-
-	if err!=nil {
-		fmt.Println(err)
-	}
-
-	overlayImage := ffmpeg.
-		Input("/Users/hartmamt/Projects/pg/videoSplitter/out/test-001.jpg").
-		Crop(
-			int(*box.Left * metaData.Width),
-			int(*box.Top * metaData.Height),
-			int(*box.Width * metaData.Width),
-			int(*box.Height * metaData.Height)).
-		Filter("boxblur", ffmpeg.Args{"10"})
-
-	err = ffmpeg.
-		Input("/Users/hartmamt/Projects/pg/videoSplitter/out/test-001.jpg").
-		//DrawBox(int(*box.Left * metaData.Width), int(*box.Top * metaData.Height), int(*box.Width * metaData.Width), int(*box.Height * metaData.Height), "red", 2).
-		//Filter("fps", ffmpeg.Args{"30/1"}).
-		Overlay(overlayImage,"", ffmpeg.KwArgs{
-			"x": int(*box.Left * metaData.Width),
-			"y": int(*box.Top * metaData.Height),
-		}).
-		Output("/Users/hartmamt/Projects/pg/videoSplitter/new-001.jpg").
-		OverWriteOutput().
-		Run()
-
-	if err != nil {
-
-		fmt.Println(err.Error())
-	}
-
-	//func ComplexFilterExample(testInputFile, testOverlayFile, testOutputFile string) *ffmpeg.Stream {
-	//	split := ffmpeg.Input(testInputFile).VFlip().Split()
-	//	split0, split1 := split.Get("0"), split.Get("1")
-	//	overlayFile := ffmpeg.Input(testOverlayFile).Crop(10, 10, 158, 112)
-	//	return ffmpeg.Concat([]*ffmpeg.Stream{
-	//	split0.Trim(ffmpeg.KwArgs{"start_frame": 10, "end_frame": 20}),
-	//	split1.Trim(ffmpeg.KwArgs{"start_frame": 30, "end_frame": 40})}).
-	//	Overlay(overlayFile.HFlip(), "").
-	//	DrawBox(50, 50, 120, 120, "red", 5).
-	//	Output(testOutputFile).
-	//	OverWriteOutput()
-	//}
-
-	//err = ffmpeg.
-	//	Input("/Users/hartmamt/Projects/pg/videoSplitter/out/test-001.jpg").
-	//	//DrawBox(int(*box.Left * metaData.Width), int(*box.Top * metaData.Height), int(*box.Width * metaData.Width), int(*box.Height * metaData.Height), "red", 2).
-	//	//Filter("fps", ffmpeg.Args{"30/1"}).
-	//	Overlay(overlayFile,"").
-	//	Output("/Users/hartmamt/Projects/pg/videoSplitter/out/new-001.jpg").
-	//	OverWriteOutput().
-	//	Run()
-
-	if err!=nil {
-		fmt.Println("after overlay")
-		fmt.Println(err)
-	}
-
-	//split := Input(TestInputFile1).VFlip().Split()
-	//split0, split1 := split.Get("0"), split.Get("1")
-	//overlayFile := Input(TestOverlayFile).Crop(10, 10, 158, 112)
-	//err := Concat([]*Stream{
-	//	split0.Trim(KwArgs{"start_frame": 10, "end_frame": 20}),
-	//	split1.Trim(KwArgs{"start_frame": 30, "end_frame": 40})}).
-	//	Overlay(overlayFile.HFlip(), "").
-	//	DrawBox(50, 50, 120, 120, "red", 5).
-	//	Output(TestOutputFile1).
-	//	OverWriteOutput().
-	//	Run()
-
-
-
-	/*
-	ffmpeg -i derpdog.mp4 -filter_complex \
-	 "[0:v]crop=200:200:60:30,boxblur=10[fg]; \
-	  [0:v][fg]overlay=60:30[v]" \
-	-map "[v]" -map 0:a -c:v libx264 -c:a copy -movflags +faststart derpdogblur.mp4
-	 */
-
-
 	//
-	//// Reassemble Video
+	//
+	//err = ffmpeg.Input("/Users/hartmamt/Projects/pg/videoSplitter/in/bezos_vogels.mp4").
+	//	Filter("aresample",ffmpeg.Args{"async=1000"}).
+	//	Output("/Users/hartmamt/Projects/pg/videoSplitter/test.mp3").
+	//	OverWriteOutput().
+	//	Run()
+	// Break Into Frames
+
 	err = ffmpeg.
-		Input(
-			"/Users/hartmamt/Projects/pg/videoSplitter/out/test*.jpg",
-			ffmpeg.KwArgs{
-				"pattern_type":"glob",
-				"framerate": 30000/1001,
-			},
-		).
-		Output("/Users/hartmamt/Projects/pg/videoSplitter/out/move.mp4").
+		Input("/Users/hartmamt/Projects/pg/videoSplitter/in/durst.mp4").
+		//Filter("fps", ffmpeg.Args{fmt.Sprintf("%f", metaData.FrameRate)}).
+		Filter("select", ffmpeg.Args{"'between(n\\,1\\,100)'"}).
+		Output("/Users/hartmamt/Projects/pg/videoSplitter/out/test-%04d.jpg", ffmpeg.KwArgs{
+			"start_number": 0,
+			//"select": "between(n\\,1\\,100)",
+		}).
 		OverWriteOutput().
 		Run()
+
+	fmt.Println(err)
+
+	return
+	/*
+	get timestamps for frames
+	 ffprobe -f lavfi -i "movie=durst.mp4,fps=fps=29.97[out0]" -show_frames -show_entries frame=pkt_pts_time -of csv=p=0 > frames.txt
+	 */
 	//
 	//if err!=nil {
 	//	fmt.Println(err)
 	//}
+
+	videoPath := "/Users/hartmamt/Projects/pg/videoSplitter/in/durst.mp4"
+	videoOutput := "/Users/hartmamt/Projects/pg/videoSplitter/out/"
+	combinedVideoPath := videoOutput +  "combined.mp4"
+//https://itectec.com/superuser/how-to-extract-the-timestamps-associated-with-frames-ffmpeg-extracts-from-a-video-with-the-r-option/
+
+	/*
+	image 0176
+	  {
+	            "Timestamp": 5038,
+	            "Person": {
+	                "Index": 1,
+	                "BoundingBox": {
+	                    "Width": 0.34453123807907104,
+	                    "Height": 0.737500011920929,
+	                    "Left": 0.08437500149011612,
+	                    "Top": 0.17916665971279144
+	                }
+	            }
+	        },
+	0.035
+	5028 * ? = 176
+	 */
+
+	//// Reassemble Video
+	video := ffmpeg.Input(
+			videoOutput + "*.jpg",
+			ffmpeg.KwArgs{
+				"pattern_type":"glob",
+				"framerate": metaData.FrameRate,
+			},
+		)
+	fmt.Println(video)
+	audio := ffmpeg.Input(videoPath)
+
+	err = ffmpeg.Concat(
+			[]*ffmpeg.Stream{video, audio,},
+			ffmpeg.KwArgs{
+				"v":1,
+				"a":1,
+			},
+		).
+		Output(combinedVideoPath).
+		OverWriteOutput().
+		Run()
+
+	if err!=nil {
+		fmt.Fprint(os.Stderr, "request cancelled\n")
+		fmt.Println(err)
+	}
+
+
 
 }
